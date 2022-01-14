2668e4437723b033731de2da23826dcd6a74da1d
Safal Bhandari <82866677+Safal009@users.noreply.github.com>
2022-01-14 13:11:00 +0530

    Create Datatypes.sol

---
+++ Datatypes.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.5.1;
+
+contract MyContract{
+    string public value ="Hello";
+    bool public  myBool = true;
+    int public myInt = -1; //Any sign
+    uint public myUint = 1; //Only positive no sign
+}
